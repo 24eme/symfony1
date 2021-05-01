1218868a6b380f9ed8d440e596440f675f9ceef5
Vincent LAURENT <vince.laurent@gmail.com>
2021-05-01 13:43:25 +0200

    Truncate text accept to truncate at middle

--- lib/helper/TextHelper.php
+++ lib/helper/TextHelper.php
@@ -32,8 +32,12 @@
  *
  * @return string
  */
-function truncate_text($text, $length = 30, $truncate_string = '...', $truncate_lastspace = false, $truncate_pattern = null, $length_max = null)
+function truncate_text($text, $length = 30, $truncate_string = '...', $mode = 'end')
 {
+  if($mode === true) {
+    $mode = 'lastspace';
+  }
+
   if ('' == $text)
   {
     return '';
@@ -72,12 +76,17 @@ function truncate_text($text, $length = 30, $truncate_string = '...', $truncate_
       }
     }
 
-    $truncate_text = $substr($text, 0, $length - $strlen($truncate_string));
-    if ($truncate_lastspace)
-    {
-      $truncate_text = preg_replace('/\s+?(\S+)?$/', '', $truncate_text);
+    if($mode === 'middle') {
+        $first_length = round(($length - $strlen($truncate_string)) / 2);
+        $last_length = $length - $strlen($truncate_string) - $first_length;
+        $text = $substr($text, 0, $first_length).$truncate_string.$substr($text, $last_length*-1);
+    } elseif ($mode === 'lastspace') {
+        $truncate_text = $substr($text, 0, $length - $strlen($truncate_string));
+        $truncate_text = preg_replace('/\s+?(\S+)?$/', '', $truncate_text);
+        $text = $truncate_text.$truncate_string;
+    } else {
+        $text = $substr($text, 0, $length - $strlen($truncate_string)).$truncate_string;
     }
-    $text = $truncate_text.$truncate_string;
   }
 
   if ($mbstring)
